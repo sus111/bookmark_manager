e3c5e15e9fc65dbac1f9f4e87b10234de79e5a20
sus111 <sgraham535@gmail.com>
2016-07-29 11:49:32 +0100

    ex 23

--- app/app.rb
+++ app/app.rb
@@ -14,6 +14,21 @@ get '/' do
   erb :'links/home'
 end
 
+get '/sessions/new' do
+  erb :'sessions/new'
+end
+
+post '/sessions' do
+  user = User.authenticate(params[:email], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    flash.now[:errors] = "The email or password is incorrect"
+    erb :'sessions/new'
+  end
+end
+
 get '/links' do
   @links =Link.all
   @tags = Tag.all
---
+++ spec/feature/sign_in_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+  feature 'sign_in' do
+
+    let!(:user) do
+      User.create(email: 'becca@gmail.com',
+                  password: 'haircut',
+                  password_confirmation: 'haircut')
+    end
+
+    scenario 'user can sign in' do
+    visit '/sessions/new'
+    fill_in "email", :with => "becca@gmail.com"
+    fill_in "password", :with => "haircut"
+    click_button('Sign in')
+    expect(page).to have_content "Welcome becca@gmail.com"
+    end
+  end
